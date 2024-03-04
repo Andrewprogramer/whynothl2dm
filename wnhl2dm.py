d9b3066af13264978db937905ad9c051a2efa53d
weapon_smg1 <114591031+Andrewprogramer@users.noreply.github.com>
2024-03-04 22:29:12 +0200

    Update wnhl2dm.py
    -Added date display in menu
    -Added new function
    -Added Debug if the cheat is run via cmd
    -Update Menu

--- wnhl2dm.py
+++ wnhl2dm.py
@@ -1,7 +1,16 @@
 from pymem import *
 from pymem.process import *
 from tkinter import *
-from tkinter import messagebox
+import datetime as dt
+from os import system as cmd
+
+
+
+#this is only in case the cheat is launched via the cmd
+cmd("title WhyNot Debug")
+cmd("cls")
+
+
 
 mem = Pymem("hl2.exe")
 game_module = module_from_name(mem.process_handle, "client.dll").lpBaseOfDll
@@ -16,11 +25,14 @@ def getPtrAddr(address, offsets):
     return addr
 
 
+
 root = Tk()
 root['bg'] = 'black'
 root.title('WhyNot HL2DM')
 root.geometry('320x240')
 root.resizable(width=False, height=False)
+
+date = dt.datetime.now()
 
 
 def WallhackOFF():
@@ -29,17 +41,23 @@ def WallhackOFF():
 def WallhackON():
         mem.write_int(getPtrAddr(game_module + 0x004BD9D0, [0x88, 0xC, 0x118, 0x4F8, 0x478]), 2)
 
-
+def DisableCustomSprayON():
+        mem.write_int(getPtrAddr(game_module + 0x004C6AB0, [0x94, 0xC, 0x118, 0x1C, 0x41C]), 1)
       
-
+def DisableCustomSprayOFF():
+        mem.write_int(getPtrAddr(game_module + 0x004C6AB0, [0x94, 0xC, 0x118, 0x1C, 0x41C]), )
 
 Twallhack = Label(root, text = "Wallhack", bg='black', fg='white' ).place(x=0,y=0,width=318,height=30)
-Twallhack = Label(root, text = "About", bg='black', fg='white').place(x=0,y=110,width=318,height=30)
-Twallhack = Label(root, text = "WhyNot Half-Life 2 DeathMatch Build 16.02.2024", bg='black', fg='white').place(x=0,y=130,width=319,height=106)
+Twallhack = Label(root, text = "DisableSprays", bg='black', fg='white').place(x=0,y=110,width=318,height=30)
+Time= Label(root, text =f"{date:%A, %B %d, %Y}", bg='black', fg='white' ).place(x=0,y=200,width=317,height=36)
 Button1 = Button(root, text ="ON", bg='#ec03fc', fg='white' , command = WallhackON)
 Button1.place(x=130,y=30,width=59,height=30)
 Button2 = Button(root, text ="OFF", bg='#ec03fc', fg='white' , command = WallhackOFF)
 Button2.place(x=130,y=70,width=60,height=30)
+Button3 = Button(root, text ="ON", bg='#ec03fc', fg='white' , command = DisableCustomSprayON)
+Button3.place(x=130,y=140,width=60,height=30)
+Button4 = Button(root, text ="OFF", bg='#ec03fc', fg='white' , command = DisableCustomSprayOFF)
+Button4.place(x=130,y=180,width=60,height=30)
 
 
 root.mainloop()
